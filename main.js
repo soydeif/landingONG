1bff0259efcff13cabdae02c3dd36db6e3a4e356
David Diaz Nava <81191528+soydeif@users.noreply.github.com>
2023-03-19 18:13:39 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    gsap.registerPlugin(ScrollTrigger);
+  
+    gsap.utils.toArray('.section').forEach(section =>{
+        ScrollTrigger.create({
+            trigger: section,
+            start:'top top',
+            pin:true,
+            pinSpacing: false,
+            
+        });
+    });
+  });
+  
